ba51e6ee58527b557e02ed101ab8c027d0518930
JaypalDhapa <jaypaldhapa25@gmail.com>
2026-02-04 18:04:41 +0530

    fist commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+//middleware
+app.use(express.json());
+
+
+app.get("/setCookie",(req,res)=>{
+    res.cookie("token","abc123",{
+        httpOnly:true,
+        secure:false,
+        
+    });
+    res.send("cookie set");
+});
+
+app.listen(3000);
